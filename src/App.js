a7ccb519040e49ba421a60f0231e1bd8e9fc3821
Ale Caceres <aleda997@gmail.com>
2021-06-04 15:52:06 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { 
+  Button,
+  ButtonGroup,
+  Container } from "react-bootstrap";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ButtonGroup aria-label="Basic example">
+        <Button variant="secondary">0</Button>
+        <Button variant="secondary">1</Button>
+        <Button variant="secondary">2</Button>
+        <Button variant="secondary">3</Button>
+      </ButtonGroup>
+    </Container>
   );
 }
 
